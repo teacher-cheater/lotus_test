08da16eda6dbff6d8925b2edac0050cbe5662bac
teacher_cheater <johnnykayfun@gmail.com>
2022-12-23 01:04:41 +0300

    add react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import Room from "./components/Room/Room";
+import Home from "./components/Home/Home";
+
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/room">Room</Link>
+              </li>
+            </ul>
+          </nav>
 
+          <Routes>
+            <Route path='/room' element={<Room />} />
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }

93e6867272b5fc7a82b85561760ca81f752ea913
teacher_cheater <johnnykayfun@gmail.com>
2022-12-23 03:43:08 +0300

    create files with members

---
+++ src/data/data.js
@@ -0,0 +1,40 @@
+export const data = {
+  informationAboutParticipants: [
+    {
+      activities: '',
+      productionTime: '80',
+      guarantee: '24',
+      termsOfPayment: 30,
+      cost: 3700000,
+      actions: '',
+      id: 1,
+    },
+    {
+      activities: '',
+      productionTime: '90',
+      guarantee: '24',
+      termsOfPayment: 100,
+      cost: 3200000,
+      actions: '',
+      id: 2,
+    },
+    {
+      activities: '',
+      productionTime: '75',
+      guarantee: '22',
+      termsOfPayment: 60,
+      cost: 2800000,
+      actions: '',
+      id: 3,
+    },
+    {
+      activities: '',
+      productionTime: '120',
+      guarantee: '36',
+      termsOfPayment: 50,
+      cost: 2500000,
+      actions: '',
+      id: 4,
+    },
+  ],
+};

e26aaed33461f78c1f359a1dd4f52fae86b5f435
teacher_cheater <johnnykayfun@gmail.com>
2022-12-25 16:14:29 +0300

    fix bugs

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     <div className={s.container}>
       <div className={s.content}>
         <h1 className={s.title}>ЛОТОС</h1>
-        <a href="http://www.lotus1.org/" target="_blank">
+        <a target="_blank" href={"http://www.lotus1.org/"}>
           <img className={s.logo} src={logo} alt="logo" />
         </a>
       </div>
